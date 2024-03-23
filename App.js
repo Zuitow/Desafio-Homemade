f9617045c71cdc21a43efdea831fe0f12007a280
PauloPZ <pc.zuit@gmail.com>
2024-03-22 22:45:24 -0300

    ue

--- App.js
+++ App.js
@@ -1,40 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
-import { estilos } from './src/styles/Styles';
-
-
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Modal,
+} from "react-native";
+import { estilos } from "./src/styles/Styles";
 
 export default function App() {
   // Consts dos textos
-  const inputalcool = "Álcool (preço por litro)."
-  const inputgasolina = "Gasolina (preço por litro)."
-  const textpribot = "Calcular"
+  const inputalcool = "Álcool (preço por litro).";
+  const inputgasolina = "Gasolina (preço por litro).";
+  const textpribot = "Calcular";
+  const textsecbot = "Calcular Novamente";
+  
+
+  // Parametros para usar a visibilidade e usar o Modal
+  const [visible, setVisible] = useState(false);
+  visModal = (x) => {
+    if (!visible) {
+      setVisible(x);
+    } else {
+      setVisible(x);
+    }
+  };
+  // Funções
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [combustivel, setCombustivel] = useState('');
+
+
+  const handlePress = () => {
+    visModal(true);
+  
+    const resultado = num1 / num2;
+    const combustivel = resultado < 0.7 ? 'Álcool' : 'Gasolina';
+
+    const compensa = "Compensa usar " + combustivel;
+    setCombustivel(compensa);
+    console.log(resultado)
+  };
   return (
     //Primeiro Container
     <View style={estilos.container}>
-
       {/* Container que é a caixa padrão */}
 
       <View style={estilos.secondaryContainer}>
-        <Image style={estilos.imagelogo} source={require('./src/assets/images/logo.png')} />
+        <Image
+          style={estilos.imagelogo}
+          source={require("./src/assets/images/logo.png")}
+        />
         <Text style={estilos.text}>Qual a Melhor Opção?</Text>
 
         {/* Container que segura os inputs */}
 
         <View style={estilos.terceiroContainer}>
           <Text style={estilos.textoinput}>{inputalcool}</Text>
-          <TextInput style={estilos.Inputs} />
+          <TextInput value={num1} style={estilos.Inputs} keyboardType="numeric"  onChangeText={setNum1}/>
           <Text style={estilos.textoinput}>{inputgasolina}</Text>
-          <TextInput style={estilos.Inputs} />
+          <TextInput value={num2} style={estilos.Inputs}  keyboardType="numeric" onChangeText={setNum2}/>
 
-          <TouchableOpacity style={estilos.bot}>
+          <TouchableOpacity onPress={() =>  handlePress()} style={estilos.bot}>
             <Text style={estilos.textobotao}>{textpribot}</Text>
           </TouchableOpacity>
         </View>
-
+        <Modal transparent={true} animationType="fade" visible={visible}>
+          {/* Segundo container2 pois é a segunda página */}
+          <View style={estilos.secondaryContainer2}>
+            <Image
+              style={estilos.imagelogo}
+              source={require("./src/assets/images/gas.png")}
+            />
+            <Text style={estilos.text2}>{combustivel}</Text>
+            <Text style={{fontSize: 20, color: 'white', textAlign: 'center'}}> Com os Preços:</Text>
+            <Text style={estilos.text3}>Álcool: R$ {num1}</Text>
+            <Text style={estilos.text3}> Gasolina: R$ {num2}</Text>
+            <TouchableOpacity onPress={() =>  visModal(false)} style={estilos.bot2}>
+            <Text style={estilos.textobotao2}>{textsecbot}</Text>
+          </TouchableOpacity>
+          </View>
+        </Modal>
       </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
--- src/styles/Styles.js
+++ src/styles/Styles.js
@@ -1,68 +1,107 @@
 import { StyleSheet } from "react-native";
 
 export const estilos = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    secondaryContainer: {
-        width: '30%',
-        height: '90%',
-        backgroundColor: '#171515',
-        borderRadius: 15,
-        justifyContent: 'center',
-        alignContent: 'center'
-    },
-    text: {
-        fontSize: 20,
-        textAlign: 'center',
-        color: 'white',
-        fontWeight: 'bold',
-        marginBottom: 25
-    },
-    imagelogo: {
-         alignSelf: 'center',
-         marginBottom: 15,
-
-    },
-    Inputs: {
-        width: '90%',
-        height: '15%',
-        borderWidth: 1,
-        borderColor: '#171515',
-        borderRadius: 5,
-        backgroundColor: 'white',
-        alignSelf: 'center',
-        margin: 5,
-    },
-    textoinput: {
-        fontSize: 20,
-        color: 'white',
-        marginLeft: 30,
-        marginTop: 15,
-        fontWeight: 'bold',
-    },
-    terceiroContainer: {
-        width: '100%',
-        height: 300,
-    },
-
-    // Eu sou um botão vermelho que escrito caucular
-    bot: {
-    backgroundColor: '#D32828',
-    width: '90%',
-    height: '15%',
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  secondaryContainer: {
+    width: "30%",
+    height: "90%",
+    backgroundColor: "#2E2E2E",
+    borderRadius: 15,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  text: {
+    fontSize: 20,
+    textAlign: "center",
+    color: "white",
+    fontWeight: "bold",
+    marginBottom: 25,
+  },
+  imagelogo: {
+    alignSelf: "center",
+    marginBottom: 15,
+  },
+  Inputs: {
+    width: "90%",
+    height: "20%",
+    borderWidth: 1,
+    borderColor: "#171515",
     borderRadius: 5,
-    alignSelf: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignSelf: "center",
+    margin: 5,
+    fontSize: 30,
+  },
+  textoinput: {
+    fontSize: 20,
+    color: "white",
+    marginLeft: 30,
     marginTop: 15,
-    },
-    textobotao: {
-        fontSize: 20,
-        color: 'white',
-        fontWeight: 'bold',
-    }
-  });
+    fontWeight: "bold",
+  },
+  terceiroContainer: {
+    width: "100%",
+    height: 300,
+  },
+
+  // Eu sou um botão vermelho que escrito caucular
+  bot: {
+    backgroundColor: "#D32828",
+    width: "90%",
+    height: "20%",
+    borderRadius: 5,
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 15,
+  },
+  textobotao: {
+    fontSize: 25,
+    color: "white",
+    fontWeight: "bold",
+  },
+  secondaryContainer2: {
+    width: "30%",
+    height: "90%",
+    backgroundColor: "#2E2E2E",
+    borderRadius: 15,
+    justifyContent: "center",
+    alignContent: "center",
+    alignSelf: "center",
+  },
+  //Sou o texto verde que fica no Modal
+  text2: {
+    color: "#6CEA39",
+    fontSize: 25,
+    textAlign: "center",
+    fontWeight: "bold",
+    marginBottom: 25,
+  },
+  //Sou o texto pequeno embaixo do código decima
+  text3: {
+    color: 'white',
+    fontSize: 15,
+    textAlign: 'center',
+  },
+  bot2: {
+    width: "65%",
+    height: "5%",
+    borderWidth: 2,
+    borderColor: "#D32828",
+    borderRadius: 5,
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 15,
+  },
+  textobotao2: {
+    fontSize: 20,
+    color: "#D32828",
+    fontWeight: "bold",
+  },
+});
